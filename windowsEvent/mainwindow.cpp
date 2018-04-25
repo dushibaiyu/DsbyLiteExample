66d105abed2e05fe252799101582263cdfabee7f
rjosodtssp <zhangzipan1@gmail.com>
2018-04-25 17:35:59 +0800

    给textEdit加过滤器，否则eventFilter不成功

--- windowsEvent/mainwindow.cpp
+++ windowsEvent/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QKeyEvent>
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     obj.setParent(this);
+    ui->textEdit->installEventFilter(this);
 }
 
 MainWindow::~MainWindow()
